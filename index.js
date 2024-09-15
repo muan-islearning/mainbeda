ddb845fca3b6fcd219ee4e8a34e96e0fbd33fd22
mua_islearning <120779191+muan-islearning@users.noreply.github.com>
2024-09-15 23:31:07 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
-};
+// audio = new Audio(''+loveaudio.src)
+funcLoveIn = 0
+
+var klik = true;
+document.getElementById("loveIn").onclick = function() {
+  if(klik){
+    if(funcLoveIn == 0){
+      funcLoveIn = 1;
+      loveIn.style = "transition:all .8s ease;transform:scale(15);opacity:0";
+      ket.style = "display:none";
+      setTimeout(() => {
+        onload()
+      }, 800);
+    }
+  }else{
+    klik = true;
+    document.getElementById("loveIn").innerHTML = '<a href="#" class="love">&#10084;</a>'
+  }
+}
+
+document.getElementById("present").onclick = function() {
+  event.preventDefault();
+  present.style = "transition:all .8s ease;transform:scale(15);opacity:0";
+  say_hi.style = "transition:all .4s ease;opacity:0";
+  hadiah.style = "transition:all .4s ease;opacity:0";
+  setTimeout(() => {
+    window.location.href = 'flower.html';
+  }, 400);
+}
+
--- typed.js
+++ typed.js
@@ -1,20 +1,32 @@
-onload = () => {
+function onload() {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
-    startAnimation();
+    startTyping();
     clearTimeout(c);
   }, 1000);
 }
 
-function startAnimation() {
+function startTyping() {
   var typed = new Typed('#say_hi', {
-    strings: ["hallo cantik..", "kamu tau ngga?", "bedanya kamu sama tv apa?", "klo tv buat ditonton", "klo kamu buat aku aja"],
+    strings: ["hallo cantik..", "kamu tau ngga?","bedanya kamu sama tv apa?", "klo tv buat ditonton", "klo kamu buat aku aja,, hehe"],
     typeSpeed: 70,
     backSpeed: 40,
     loop: false,
     onComplete: (self) => {
-      window.location.href = 'flower.html'
+      setTimeout(() => {
+        kangenTyping()
+      }, 800);
     }
   });
 }
 
+function kangenTyping(){
+  var typed = new Typed('#hadiah',{
+    strings: ["btw, aku punya sesuatu lhoo buat kamu👉👈"],
+    typeSpeed: 40,
+    loop: false,
+    onComplete: (self) => {
+      present.style = "transition:all .8s ease;opacity:1;transform:translateY(20px)";
+    }
+  })
+}
